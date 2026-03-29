57c3ed29badbf81f5d114b6fbd0e230b944a979f
Gugulothu Santhosh <santhoshgugulothu56@gmail.com>
2026-03-29 11:59:09 +0530

    implement embeddings

--- src/embeddings/embedder.py
+++ src/embeddings/embedder.py
@@ -1,1 +1,33 @@
 # create embeddings 
+from sentence_transformers import SentenceTransformer
+
+
+class Embedder:
+
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        """
+        Initialize embedding model
+        """
+
+        self.model = SentenceTransformer(model_name)
+
+    def embed_documents(self, chunks):
+        """
+        Convert chunks into embeddings
+        """
+
+        texts = [chunk.page_content for chunk in chunks]
+
+        embeddings = self.model.encode(
+            texts,
+            show_progress_bar=True
+        )
+
+        return embeddings
+
+    def embed_query(self, query):
+        """
+        Embed user query
+        """
+
+        return self.model.encode(query)
